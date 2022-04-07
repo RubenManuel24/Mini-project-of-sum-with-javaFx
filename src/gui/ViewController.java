64500202a15730591900bd1ed98642564855a188
Ruben Manuel <rubenadilsonsapalomanuel@gmail.com>
2022-04-08 00:50:36 +0100

    Criação do mini projecto

---
+++ src/gui/ViewController.java
@@ -0,0 +1,20 @@
+package gui;
+
+import javax.sound.sampled.AudioFileFormat.Type;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+
+public class ViewController {
+
+	@FXML
+	private Button btTest;
+	
+	@FXML
+	public void onBtTestAction() {
+		Alerts.showAlert("Alert title", null, "Hello", AlertType.ERROR);
+	}
+	
+}
